ede36aa75a511d70e382a47b379bd0677b952fdd
= <daniel.lervik@aland.net>
2010-09-30 13:01:00 +0200

    kul

--- ReliableSender.java
+++ ReliableSender.java
@@ -102,3 +102,65 @@ public class ReliableSender extends SenderProtocol {
 		timer.start(timeoutValue);
 	}
 }
+
+
+
+
+/**
+	Answers to all funny questions
+	
+	Task 2:
+	The given stop-and-wait protocol sends one data packet at a time. Calculate the total transfer time of 100 data packets.
+		Consider that it takes 0,001s before all bits are on the physical layer.
+		Then it takes 0,01s for each bit to travel to the receiver from the sender or vice versa.
+		
+		The sender has transmission delay of 0,001s to send a packet, 
+		and we assume the ack packet takes 0,001s to put on the channel.
+		
+		Each way takes 0,01s to travel.
+		
+		==> 100 * 2 * (0,01 + 0,001) = 2,2s
+	
+	Run a simulation and compare the result with your calculation. Do you get the same total transfer time? If not, why?
+		It is correct assuming no packet loss
+		
+	
+	
+	Task 4
+	Calculate the "optimal" minimum window size that gives the shortest possible total transfer time.
+		We want to continiously push bits on the physical layer 
+		==> 1 + Number of packets that can fit on one propagation delay
+		
+		We also get ack packets
+		==> double up the window size
+		
+		2 * ((propagationDelay / transferDelay) + 1)
+		
+		In the specific case:
+		2 * ((0,01 / 0,001) + 1) = 22
+
+	Run a simulation with the optimal window size that you have calculated.
+	Also, run simulations with smaller and larger window sizes and compare the total transfer times.
+	Remember to set the number of data chunks to more than twice the maximum window size you test. 
+	Why is this important? 
+		We need to be sure to get a steady state. By doubling the packets we are sure that we are steady.
+	What are your conclusions?
+		We should not have too large window, since there will be overhead then.
+	
+	Calculate the "optimal" timeout value, if the channel were to drop packets.
+		Assuming a static route we know when we should receive the packet ack. 
+		If we haven't got it by then, we just timeout.
+		
+		Calculation (assuming the timer is started after the packet is on the physical layer):
+		2*(transmission delay + propagation delay)
+		
+	What would be the effect of using a longer timeout value? 
+		Say that the ack pack for one packet got lost, but all data has been received.
+		Then we don't have a need for a timeout since we will get a ack pack for the next data sent.
+		We then know that all data has been received and we don't need to timeout.
+		
+		The downside is longer response times.
+			
+	What would be the effect of using a shorter timeout value?
+		Then we will always timeout for every packet sent.
+ */
